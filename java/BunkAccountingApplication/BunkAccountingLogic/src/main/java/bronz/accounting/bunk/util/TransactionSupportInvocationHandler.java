628b76be8f913f2576c7f6b602b2affda5f03203
mathewprabin@yahoo.co.in <mathewprabin@yahoo.co.in>
2022-03-17 01:36:02 -0700

    new cp + error msg fix

--- java/BunkAccountingApplication/BunkAccountingLogic/src/main/java/bronz/accounting/bunk/util/TransactionSupportInvocationHandler.java
+++ java/BunkAccountingApplication/BunkAccountingLogic/src/main/java/bronz/accounting/bunk/util/TransactionSupportInvocationHandler.java
@@ -1,10 +1,12 @@
 package bronz.accounting.bunk.util;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import bronz.accounting.bunk.framework.dao.TxnManager;
 import bronz.accounting.bunk.framework.exceptions.BunkMgmtException;
+import bronz.accounting.bunk.framework.exceptions.BunkValidationException;
 
 /**
  * Created by pmathew on 1/30/16.
@@ -29,6 +31,11 @@ public class TransactionSupportInvocationHandler implements InvocationHandler {
                 transactionManager.commit();
             } catch (Throwable t) {
                 transactionManager.rollback();
+                if (t instanceof BunkValidationException) {
+                    throw t;
+                } else if (t instanceof InvocationTargetException && t.getCause() != null && t.getCause() instanceof BunkValidationException) {
+                    throw t.getCause();
+                }
                 String failureText = "DB operation failed";
                 if(annotation != null) {
                     failureText = annotation.failureExceptionText();
